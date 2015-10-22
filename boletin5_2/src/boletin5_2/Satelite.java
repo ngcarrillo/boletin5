08e185304d6dde65d44ecfd4e9e23011e00704d2
ngarciacarrillo <ngarciacarrillo@atanasoff05.danielcastelao.org>
2015-10-22 12:05:34 +0200

    coche

---
+++ boletin5_2/src/boletin5_2/Satelite.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boletin5_2;
+
+/**
+ *
+ * @author ngarciacarrillo
+ */
+public class Satelite {
+ 
+     private double meridiano ;
+     private double paralelo;
+     private double distanciaTerra ;
+
+ public Satelite ( ) {
+
+  meridiano = paralelo = distanciaTerra = 0 ;
+}
+
+public Satelite ( double m, double p , double d ){
+
+meridiano = m ;
+paralelo = p;
+distanciaTerra = d ;
+
+}
+public void verPosicion ( ) {
+
+System.out.println (" O satelite atopase no paralelo " +paralelo +" meridiano " +meridiano +"a unha distancia da terra "+distanciaTerra ); 
+    
+}
+}
